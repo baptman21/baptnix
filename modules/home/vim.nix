fbfe60f5923b58d53592ebed81e1b6dfe5c35162
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2022-12-23 13:26:13 +0100

    feat(vim): add french dictionary

--- modules/home/vim.nix
+++ modules/home/vim.nix
@@ -1,10 +1,18 @@
 { config, pkgs, ... }:
 # Vim configuration
+let
+  vim-spell-fr-utf8-dictionary = builtins.fetchurl {
+    url = "http://ftp.vim.org/vim/runtime/spell/fr.utf-8.spl";
+    sha256 = "abfb9702b98d887c175ace58f1ab39733dc08d03b674d914f56344ef86e63b61";
+  };
+in
 {
   programs.vim = {
     enable = true;
     extraConfig = builtins.readFile ../../assets/config/vimrc;
   };
+  # French dictionary
+  home.file.".vim/spell/fr.utf-8.spl".source = vim-spell-fr-utf8-dictionary;
   # Vim files in home
   # Settings for CocNvim plugin
   home.file.coc-settings = {
