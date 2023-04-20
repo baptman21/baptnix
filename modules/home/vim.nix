28deea4fcd7517715bca8ac4fd3902b59f0921d3
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2023-04-20 20:36:02 +0200

    feat(vim): move config to vim dir

--- modules/home/vim.nix
+++ modules/home/vim.nix
@@ -9,14 +9,14 @@ in
 {
   programs.vim = {
     enable = true;
-    extraConfig = builtins.readFile ../../assets/config/vimrc;
+    extraConfig = builtins.readFile ../../assets/config/vim/vimrc;
   };
   # French dictionary
   home.file.".vim/spell/fr.utf-8.spl".source = vim-spell-fr-utf8-dictionary;
   # Vim files in home
   # Settings for CocNvim plugin
   home.file.coc-settings = {
-    text = builtins.readFile ../../assets/config/coc-settings.json;
+    text = builtins.readFile ../../assets/config/vim/coc-settings.json;
     target = ".vim/coc-settings.json";
   };
   # Vim plug to install other plugins, I currently prefer to rely on vim-plug
