be64f313332f68af3d5f6053a3f2e3b36b4242af
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2023-07-01 18:42:10 +0200

    feat(git): add shortcut for git mainly to clean branches

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -10,6 +10,9 @@
       b = "branch";
       cb = "checkout -b";
       ch = "checkout";
+      cc = "commit";
+      ca = "commit --amend";
+      ce = "commit --amend --no-edit";
       mgt = "mergetool";
       pushft = "push --follow-tags";
       pushwl = "push --force-with-lease";
@@ -17,6 +20,7 @@
       alog = "log --oneline --graph --all --decorate";
       tlog = "log --oneline --graph --decorate -n 15";
       nlog = "log --oneline --graph --decorate";
+      brclean = ''!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do echo git branch -D "$branch"; done'';
     };
 
     userName = "Baptiste Bourdet";
