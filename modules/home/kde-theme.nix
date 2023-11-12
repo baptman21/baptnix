34587be10ad4b991ead4b0a7a92916dc5b30a171
Baptiste Bourdet <bat.bourdet@gmail.com>
2023-11-12 18:32:26 +0100

    feat(kde-theme-custom): set color for desktoptheme

--- modules/home/kde-theme.nix
+++ modules/home/kde-theme.nix
@@ -8,14 +8,17 @@ let
   colors = {
     tempest = {
       primary = "#0458cf";
+      secondary = "#03336D";
       primary_rgb = "4,88,207";
     };
     typhoon = {
       primary = "#a51fff";
+      secondary = "#5E1191";
       primary_rgb = "165,31,255";
     };
     whirlwind = {
       primary = "#11d116";
+      secondary = "#09730d";
       primary_rgb = "17,209,22";
     };
   };
@@ -26,12 +29,42 @@ let
     src = ../../assets/kde-theme-custom/colorschemes/NordicDarker.colors.tmpl;
     theme_color = colors.${theme}.primary_rgb;
   };
+
+  # 8fbcbb
+  desktoptheme_with_theme = pkgs.stdenvNoCC.mkDerivation rec {
+    name = "desktoptheme_with_theme-${theme}";
+
+    phases = "buildPhase";
+
+    buildInputs = with pkgs; [
+      gnutar
+      gzip
+    ];
+
+    buildPhase = ''
+      cp -r ${../../assets/kde-theme-custom/plasma/desktoptheme/Nordic-darker} ./Nordic
+
+      chmod u+w -R ./Nordic
+
+      mkdir tmp
+      mv ./Nordic/widgets/*.svg  ./tmp/
+      gunzip -S .svgz ./Nordic/widgets/*.svgz
+
+      sed -i 's/#8fbcbb/${colors.${theme}.secondary}/g' ./Nordic/widgets/* ./tmp/*
+
+      gzip -S .svgz ./Nordic/widgets/*
+      mv ./tmp/* ./Nordic/widgets/
+
+      mv ./Nordic $out
+    '';
+  };
 in
 {
   home.file.".local/share/color-schemes/NordicDarker.colors".source = colors_with_theme;
 
+  home.file.".local/share/plasma/desktoptheme/Nordic-darker".source = desktoptheme_with_theme;
+
   home.file.".local/share/aurorae/themes/Nordic".source = ../../assets/kde-theme-custom/aurorae/Nordic;
-  home.file.".local/share/plasma/desktoptheme/Nordic-darker".source = ../../assets/kde-theme-custom/plasma/desktoptheme/Nordic-darker;
   home.file.".local/share/plasma/look-and-feel/Nordic-darker".source = ../../assets/kde-theme-custom/plasma/look-and-feel/Nordic-darker;
   home.file.".local/share/icons/Nordic-darker".source = ../../assets/kde-theme-custom/folders/Nordic-darker;
   home.file.".icons/Nordic-cursors".source = ../../assets/kde-theme-custom/cursors/Nordic-cursors;
