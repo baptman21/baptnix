ab8d5fcc6175e3d0e7a1623ad18031acc88952e1
Baptiste Bourdet <bat.bourdet@gmail.com>
2023-11-12 18:52:28 +0100

    feat(kde-theme): use git clone instead of having copy
    Avoid a copy of the theme in the repo and only have needed files.

--- assets/kde-theme-custom/plasma/desktoptheme/Nordic-darker/README.md
+++
@@ -1,5 +0,0 @@
-Fork of the [Nord Darker](https://github.com/EliverLara/Nordic-kde) repo.
-
-### A dark theme created using the awesome [Nord](https://github.com/arcticicestudio/nord) color pallete.
-
-![Nordic preview](preview/preview.png)
--- modules/home/kde-theme.nix
+++ modules/home/kde-theme.nix
@@ -26,23 +26,48 @@ in
 { config, pkgs, ... }:
 let
   colors_with_theme = pkgs.substituteAll {
-    src = ../../assets/kde-theme-custom/colorschemes/NordicDarker.colors.tmpl;
+    src = ../../assets/kde-theme-custom/NordicDarker.colors.tmpl;
     theme_color = colors.${theme}.primary_rgb;
   };
 
-  # 8fbcbb
-  desktoptheme_with_theme = pkgs.stdenvNoCC.mkDerivation rec {
-    name = "desktoptheme_with_theme-${theme}";
+  NordicDarkerGit = pkgs.stdenv.mkDerivation {
+    name = "nordic-darker";
 
-    phases = "buildPhase";
+    src = pkgs.fetchFromGitHub {
+      owner = "EliverLara";
+      repo = "Nordic";
+      rev = "master";
+      sha256 = "sha256-cH/ZWZxKuJOPUafinAyX7/CrUfpo+lWGX7HraIYPsUU=";
+    };
 
-    buildInputs = with pkgs; [
-      gnutar
-      gzip
-    ];
+    nativeBuildInputs = [ ];
 
     buildPhase = ''
-      cp -r ${../../assets/kde-theme-custom/plasma/desktoptheme/Nordic-darker} ./Nordic
+      cp -r $src ./Nordic
+
+      chmod u+w -R ./Nordic
+
+      cp -r ${../../assets/kde-theme-custom/layouts} ./Nordic/kde/plasma/look-and-feel/Nordic-darker/contents/layouts
+
+      cp -r ./Nordic $out
+    '';
+  };
+
+  NordicDarkerKdeGit = pkgs.stdenv.mkDerivation {
+    name = "nordic-darker";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "EliverLara";
+      repo = "Nordic-kde";
+      rev = "master";
+      sha256 = "sha256-td3zATfLCEbz48QNR0GYyMB5TuOg9XkpbmebT1pznkw=";
+    };
+
+    nativeBuildInputs = [ pkgs.gzip ];
+
+    # Replace the color by the theme color in the svg of the theme
+    buildPhase = ''
+      cp -r $src ./Nordic
 
       chmod u+w -R ./Nordic
 
@@ -62,11 +87,11 @@ in
 {
   home.file.".local/share/color-schemes/NordicDarker.colors".source = colors_with_theme;
 
-  home.file.".local/share/plasma/desktoptheme/Nordic-darker".source = desktoptheme_with_theme;
+  home.file.".local/share/plasma/desktoptheme/Nordic-darker".source = NordicDarkerKdeGit;
 
-  home.file.".local/share/aurorae/themes/Nordic".source = ../../assets/kde-theme-custom/aurorae/Nordic;
-  home.file.".local/share/plasma/look-and-feel/Nordic-darker".source = ../../assets/kde-theme-custom/plasma/look-and-feel/Nordic-darker;
-  home.file.".local/share/icons/Nordic-darker".source = ../../assets/kde-theme-custom/folders/Nordic-darker;
-  home.file.".icons/Nordic-cursors".source = ../../assets/kde-theme-custom/cursors/Nordic-cursors;
+  home.file.".local/share/aurorae/themes/Nordic".source = NordicDarkerGit + "/kde/aurorae/Nordic";
+  home.file.".local/share/plasma/look-and-feel/Nordic-darker".source = NordicDarkerGit + "/kde/plasma/look-and-feel/Nordic-darker";
+  home.file.".local/share/icons/Nordic-darker".source = NordicDarkerGit + "/kde/folders/Nordic-darker";
+  home.file.".icons/Nordic-cursors".source = NordicDarkerGit + "/kde/cursors/Nordic-cursors";
 
 }
