c5ba3067935bca1fc5755a9d6b2b49e54bf9693f
Baptiste Bourdet <bat.bourdet@gmail.com>
2024-01-08 11:05:52 +0100

    fix(typhoon): alias for kubectl-view-secret

--- homes/typhoon.nix
+++ homes/typhoon.nix
@@ -57,7 +57,7 @@
 
   home.shellAliases = {
     k = "kubectl";
-    kvs = "kubectl view-secret";
+    kvs = "kubectl-view-secret";
     kex = "k exec -it";
     ky = "k -o yaml";
     kg = "k get";
