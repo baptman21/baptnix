95a57edaea40da2b5e750043d462453d7492b6f0
Baptiste Bourdet <bat.bourdet@gmail.com>
2024-02-20 16:44:15 +0100

    fix(lvim: after: terraform): tabstop for tf files

--- assets/config/lvim/after/ftplugin/terraform.lua
+++ assets/config/lvim/after/ftplugin/terraform.lua
@@ -1,3 +1,5 @@
+vim.cmd("setlocal shiftwidth=2 tabstop=2")
+
 require("lvim.lsp.manager").setup("terraformls", {
     cmd = { "terraform-ls", "serve", "-log-file=/tmp/terraform-ls-{{pid}}.log" }
 })
