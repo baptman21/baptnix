8c817422f9ea1adb6f0f0559f265c7a1d86bea0a
Baptiste Bourdet <bat.bourdet@gmail.com>
2024-08-18 18:56:51 +0200

    feat(lazyvim): add treesitter config

---
+++ assets/config/lazyvim/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+            table.insert(opts.ensure_installed, "go")
+            table.insert(opts.ensure_installed, "helm")
+        end,
+    }
+}
