6ed3e63dfe06f10a3241d0bab69310cbfabb6820
Baptiste Bourdet <bat.bourdet@gmail.com>
2024-09-24 13:32:39 +0200

    fix(typhoon: alias): add full cmd to print

--- homes/typhoon.nix
+++ homes/typhoon.nix
@@ -64,8 +64,8 @@
     k = "kubectl";
     kvs = "kubectl-view-secret";
     kex = "k exec -it";
-    ksh = "pod=$(kgp | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- /bin/sh";
-    kbh = "pod=$(kgp | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- bash";
+    ksh = "pod=$(kgp | fzf | awk '{print $1}'); print -rs kex $pod -- /bin/sh; kubectl exec -it $pod -- /bin/sh";
+    kbh = "pod=$(kgp | fzf | awk '{print $1}'); print -rs kex $pod -- bash; kubectl exec -it $pod -- bash";
     ky = "k -o yaml";
     kg = "k get";
     kwg = "watch -n 2 -- kubectl get";
@@ -76,12 +76,12 @@
     kns = "kubens";
     tf = "terraform";
     by = "bat -l yaml";
-    kdbb = "pod=$(kgp | grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- bash";
-    kdbp = "pod=$(kgp | grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- psql";
-    kdbmab = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=master'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- bash";
-    kdbmap = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=master'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- psql";
-    kdbreb = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=replica'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- bash";
-    kdbrep = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=replica'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod; kubectl exec -it $pod -- psql";
+    kdbb = "pod=$(kgp | grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod -- bash; kubectl exec -it $pod -- bash";
+    kdbp = "pod=$(kgp | grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod -- psql; kubectl exec -it $pod -- psql";
+    kdbmab = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=master'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod -- bash; kubectl exec -it $pod -- bash";
+    kdbmap = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=master'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod -- psql; kubectl exec -it $pod -- psql";
+    kdbreb = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=replica'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod -- bash; kubectl exec -it $pod -- bash";
+    kdbrep = "pod=$(kgp --selector='postgres-operator.crunchydata.com/role=replica'| grep -- '-pg-' | fzf | awk '{print $1}'); print -rs kex $pod -- psql; kubectl exec -it $pod -- psql";
     kgetall = "kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found";
   };
 
