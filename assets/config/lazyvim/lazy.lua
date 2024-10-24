fada55070e1234d12f2c948c9d29c5f8930e90e1
Baptiste Bourdet <bat.bourdet@gmail.com>
2024-10-24 11:08:41 +0200

    feat(lazyvim): add copilot

--- assets/config/lazyvim/lazy.lua
+++ assets/config/lazyvim/lazy.lua
@@ -18,9 +18,10 @@ require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-{ import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.util.project" },
+        { import = "lazyvim.plugins.extras.dap.core" },
+        { import = "lazyvim.plugins.extras.coding.copilot" },
+        { import = "lazyvim.plugins.extras.test.core" },
+        { import = "lazyvim.plugins.extras.util.project" },
         -- import/override with your plugins
         { import = "plugins" },
     },
