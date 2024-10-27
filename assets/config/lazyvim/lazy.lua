8567554a11beab607ae7f92adce7cc78670f3402
Baptiste Bourdet <bat.bourdet@gmail.com>
2024-10-27 23:50:49 +0100

    feat(lazy): add octo

--- assets/config/lazyvim/lazy.lua
+++ assets/config/lazyvim/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.nix" },
+		{ import = "lazyvim.plugins.extras.util.octo" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
