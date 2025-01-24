cc111adc2526f34774ca95924cef25500cc62b7e
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-01-24 09:31:37 +0100

    refacto(lazyvim: plugins): format files

--- assets/config/lazyvim/plugins/mason.lua
+++ assets/config/lazyvim/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-        opts.ensure_installed = {}
-    end,
+	"williamboman/mason.nvim",
+	opts = function(_, opts)
+		opts.ensure_installed = {}
+	end,
 }
