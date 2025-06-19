7a0876ad30973d5ed9795a4336bd2fbdf6da1bc7
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-06-19 14:44:25 +0200

    fix: use awscli2

--- homes/storm.nix
+++ homes/storm.nix
@@ -68,7 +68,7 @@
     '';
 
     home.packages =
-      [ pkgs.teleport pkgs.awscli pkgs.azure-cli pkgs.choose-gui ];
+      [ pkgs.teleport pkgs.awscli2 pkgs.azure-cli pkgs.choose-gui ];
 
     # SSH is setup manually because config is different
     home.file.".config/git/databricks_config".text = ''
