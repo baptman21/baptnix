a63d8b0e5761d30806da3d8776e8a9f9a232bf6c
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-07-05 22:06:38 +0200

    fix(lazyvim): background transparent

--- assets/config/lazyvim/plugins/colorscheme.lua
+++ assets/config/lazyvim/plugins/colorscheme.lua
@@ -3,13 +3,13 @@ return {
 		"navarasu/onedark.nvim",
 		opts = {
 			style = "darker",
+			transparent = true,
 			colors = {
 				-- Iris colors, cf ../../../themes/iris
 				green = "#36b32d",
 				purple = "#956ad3",
 				yellow = "#e1a51c",
 				cyan = "#1dcae0",
-				bg_d = "#272537", -- set same color as background for transparency
 			},
 			highlights = {
 				DashboardHeader = { fg = "$purple" }, -- color for dashboard-nvim
