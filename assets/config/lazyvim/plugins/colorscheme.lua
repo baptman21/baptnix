5b16b23912de4b87ce0b5621d8746c58b8d71ab0
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-07-05 18:44:44 +0200

    feat(colorscheme): adapt colorscheme

--- assets/config/lazyvim/plugins/colorscheme.lua
+++ assets/config/lazyvim/plugins/colorscheme.lua
@@ -4,10 +4,10 @@ return {
 		opts = {
 			colors = {
 				-- Iris colors, cf ../../../themes/iris
-				green = "#71ab3a",
+				green = "#36b32d",
 				purple = "#956ad3",
 				yellow = "#e1a51c",
-				cyan = "#6ab6bd",
+				cyan = "#1dcae0",
 			},
 			highlights = {
 				DashboardHeader = { fg = "$purple" }, -- color for dashboard-nvim
