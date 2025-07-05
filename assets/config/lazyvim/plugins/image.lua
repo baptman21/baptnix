5343c2cb41a7f0ae24ca15e6f7ab69d5c56d663c
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-07-05 22:03:57 +0200

    fix(lazyvim: image.nvim): disable if KITTY_SCROLLBACK_NVIM

--- assets/config/lazyvim/plugins/image.lua
+++ assets/config/lazyvim/plugins/image.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"3rd/image.nvim",
+		cond = vim.env.KITTY_SCROLLBACK_NVIM ~= "true",
 		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
 		opts = {
 			processor = "magick_cli",
