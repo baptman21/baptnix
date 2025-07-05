bcb4376416f2c1b0d08d1d58f593612cf32f3600
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-07-05 18:34:11 +0200

    feat(lazyvim: onedark): use iris colors overrides

--- assets/config/lazyvim/plugins/colorscheme.lua
+++ assets/config/lazyvim/plugins/colorscheme.lua
@@ -2,6 +2,13 @@ return {
 	{
 		"navarasu/onedark.nvim",
 		opts = {
+			colors = {
+				-- Iris colors, cf ../../../themes/iris
+				green = "#71ab3a",
+				purple = "#956ad3",
+				yellow = "#e1a51c",
+				cyan = "#6ab6bd",
+			},
 			highlights = {
 				DashboardHeader = { fg = "$purple" }, -- color for dashboard-nvim
 			},
