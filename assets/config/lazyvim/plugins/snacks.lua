c1f4e0f7de4371fd08fb381c012a8da4f7fb0631
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-07-08 18:08:02 +0200

    feat(lazyvim: snacks): map to see ignored

--- assets/config/lazyvim/plugins/snacks.lua
+++ assets/config/lazyvim/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
 					input = {
 						keys = {
 							["<c-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+							["<c-i>"] = { "toggle_ignored", mode = { "i", "n" } },
 							["<c-w>"] = { "cycle_win", mode = { "i", "n" } },
 						},
 					},
