4ea2ccc8e1b603fa40c11ec121b9c6fa2c5628d8
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-08-01 16:56:09 +0200

    feat(lazyvim): add fugitive command to list

--- assets/config/lazyvim/plugins/git.lua
+++ assets/config/lazyvim/plugins/git.lua
@@ -5,6 +5,7 @@ return {
 			"G",
 			"Git",
 			"Gdiffsplit",
+			"Gvdiffsplit",
 			"Gread",
 			"Gwrite",
 			"Ggrep",
@@ -17,5 +18,10 @@ return {
 			"Gedit",
 		},
 		ft = { "fugitive" },
+		keys = {
+			{ "<leader>g2", "<cmd>diffget //2<CR>", desc = "Get the change on the left." },
+			{ "<leader>g3", "<cmd>diffget //3<CR>", desc = "Get the change on the right." },
+			{ "<leader>g4", "<cmd>diffput<CR>", desc = "Get the change off the diff (diffput)." },
+		},
 	},
 }
