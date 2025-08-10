f10b17b593af6eafd692059978b791420f8593f2
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-08-10 17:14:54 +0200

    feat(mason): disable debugpy

--- assets/config/lazyvim/plugins/mason.lua
+++ assets/config/lazyvim/plugins/mason.lua
@@ -1,6 +1,16 @@
 return {
-	"williamboman/mason.nvim",
-	opts = function(_, opts)
-		opts.ensure_installed = {}
-	end,
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = {}
+		end,
+	},
+
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = {}
+			opts.automatic_installation = false
+		end,
+	},
 }
