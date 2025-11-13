29c6dd879bd27d47158085628dae31d220a16a65
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-11-13 20:47:39 +0100

    feat(lazyvim): setting for kitty-scrollback

--- assets/config/lazyvim/plugins/kitty_scrollback.lua
+++ assets/config/lazyvim/plugins/kitty_scrollback.lua
@@ -12,7 +12,39 @@ return {
 		-- version = '*', -- latest stable version, may have breaking changes if major version changed
 		-- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
 		config = function()
-			require("kitty-scrollback").setup()
+			require("kitty-scrollback").setup({
+				{
+					-- KsbCallbacks? fire and forget callback functions
+					callbacks = {
+						after_ready = function()
+							vim.opt.number = true
+							vim.opt.relativenumber = true
+						end,
+					},
+					keymaps_enabled = false,
+					restore_options = false,
+					-- KsbStatusWindowOpts? options for status window indicating that kitty-scrollback.nvim is ready
+					status_window = {
+						-- boolean If true, show status window in upper right corner of the screen
+						enabled = true,
+						-- boolean If true, use plaintext instead of nerd font icons
+						style_simple = false,
+						-- boolean If true, close the status window after kitty-scrollback.nvim is ready
+						autoclose = false,
+						-- boolean If true, show a timer in the status window while kitty-scrollback.nvim is loading
+						show_timer = false,
+						-- KsbStatusWindowIcons? Icons displayed in the status window
+						icons = {
+							-- string kitty status window icon
+							kitty = "󰄛",
+							-- string heart string heart status window icon
+							heart = "󰣐", -- variants 󰣐 |  |  | ♥ |  | 󱢠 | 
+							-- string nvim status window icon
+							nvim = "", -- variants  |  |  | 
+						},
+					},
+				},
+			})
 		end,
 	},
 }
