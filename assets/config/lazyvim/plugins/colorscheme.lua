b33fe7a5af69ce3f69ee8f2d1b0f3bf730b46ce0
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-11-20 11:55:19 +0100

    fix(onedark): comment highlights

--- assets/config/lazyvim/plugins/colorscheme.lua
+++ assets/config/lazyvim/plugins/colorscheme.lua
@@ -5,6 +5,10 @@ return {
 			style = "dark",
 			transparent = true,
 			highlights = {
+				-- TODO: remove when fixed
+				-- Cf. https://github.com/navarasu/onedark.nvim/issues/251#issuecomment-3544178336
+				["@nospell"] = { fg = "none" },
+				["@spell"] = { fg = "none" },
 				SnacksDashboardHeader = { fg = "$purple" }, -- color for snacks dashboard
 
 				RenderMarkdownH1Bg = { bg = "$purple" },
