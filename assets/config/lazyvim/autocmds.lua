fa7ee16bb3acb34d425f63e10676a84d2b930f91
Baptiste Bourdet <bat.bourdet@gmail.com>
2025-12-08 13:27:18 +0100

    fix(blink.cmp): autowrap issue

--- assets/config/lazyvim/autocmds.lua
+++ assets/config/lazyvim/autocmds.lua
@@ -1,1 +1,24 @@
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Fix issue with blink.cmp autowrap
+-- Cf. https://github.com/saghen/blink.cmp/issues/1445?reload=1
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BlinkCmpMenuOpen",
+	callback = function()
+		local formatoptions = vim.opt.formatoptions:get()
+		if formatoptions.t then
+			vim.b.restore_formatoptions_t = true
+			vim.opt.formatoptions:remove("t")
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BlinkCmpMenuClose",
+	callback = function()
+		if vim.b.restore_formatoptions_t then
+			vim.opt.formatoptions:append("t")
+			vim.b.restore_formatoptions_t = nil
+		end
+	end,
+})
