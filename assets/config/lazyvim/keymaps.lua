0841f798e308a5a2b72d4c2d0fd459e99c91eaf3
Baptiste Bourdet <bat.bourdet@gmail.com>
2026-03-03 10:41:41 +0100

    fix(lazyvim): resize vertical keymaps

--- assets/config/lazyvim/keymaps.lua
+++ assets/config/lazyvim/keymaps.lua
@@ -1,8 +1,8 @@
 local map = vim.keymap.set
 
 -- Resize window arrow keys
-map("n", "<Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<Up>", "<cmd>resize +1<cr>", { desc = "Increase Window Height" })
+map("n", "<Down>", "<cmd>resize -1<cr>", { desc = "Decrease Window Height" })
 map("n", "<Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
